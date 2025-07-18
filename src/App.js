90c8e478c8680fba002b4a83b1067ca04d2ca457
Prerak yadav <prerakyadav@Preraks-MacBook-Air.local>
2025-07-18 13:13:20 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,36 +1,99 @@
 import "./App.css";
 import Navbar from "./Components/common/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import Home from "./pages/Home";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import Signup from "./pages/Signup"
-import PrivateRoute from "./Components/PrivateRoute";
+import PrivateRoute from "./Components/core/Auth/PrivateRoute";
 import VerifyEmail from "./pages/VerifyEmail";
-
+import ForgotPassword from "./pages/ForgotPassword";
+import OpenRoute from "./Components/core/Auth/OpenRoute";
+import UpdatePassword from "./pages/UpdatePassword";
+import MyProfile from "./Components/core/Dashboard/MyProfile";
+import { useDispatch } from "react-redux";
+import Error from "./pages/Error";
+import { ACCOUNT_TYPE } from "./utils/constants";
+import Cart from "./Components/core/Dashboard/Cart";
+import Settings from "./Components/core/Dashboard/Settings";
+import EnrolledCourses from "./Components/core/Dashboard/EnrolledCourses";
+import AddCourse from "./Components/core/Dashboard/AddCourse";
+import MyCourses from "./Components/core/Dashboard/MyCourses";
+// import EditCourse from "./Components/core/Dashboard/EditCourse";
+// import Instructor from "./Components/core/Dashboard/InstructorDashboard/Instructor";
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.profile);
 
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+          <Route
+          path="update-password/:token"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+         <Route path="/about" element={<About/>} />
+         <Route path="/contact" element={<Contact />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/dashboard" element={
-          <PrivateRoute isLoggedIn={isLoggedIn}>
-            <Dashboard />
-          </PrivateRoute>
-        } />
+        <Route
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+
+          <Route path="dashboard/Settings" element={<Settings />} />
+
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route path="dashboard/cart" element={<Cart />} />
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+            </>
+          )}
+
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+             {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
+              <Route path="dashboard/add-course" element={<AddCourse />} /> 
+              <Route path="dashboard/my-courses" element={<MyCourses />} />
+              {/* <Route
+                path="dashboard/edit-course/:courseId"
+                element={<EditCourse />}
+              /> */}
+            </>
+          )}
+        </Route>
+        <Route path="*" element={<Error/>}/>
       </Routes>
     </div>
   )
---
+++ src/Components/common/ReviewSlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReviewSlider = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default ReviewSlider
